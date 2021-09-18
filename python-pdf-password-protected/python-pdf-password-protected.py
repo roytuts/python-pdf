07709338b3fb59245a6c04987096104534b5bf0c
roytuts <projwebinfo@gmail.com>
2021-09-18 11:36:41 +0530

    fixed issue for multiple pages

--- python-pdf-password-protected/python-pdf-password-protected.py
+++ python-pdf-password-protected/python-pdf-password-protected.py
@@ -1,19 +1,20 @@
 import PyPDF2
 
-pdf_in_file = open("simple.pdf",'rb')
+#pdf_in_file = open("simple.pdf",'rb')
+pdf_in_file = open("gre_research_validity_data.pdf",'rb')
 
 inputpdf = PyPDF2.PdfFileReader(pdf_in_file)
 pages_no = inputpdf.numPages
+output = PyPDF2.PdfFileWriter()
 
 for i in range(pages_no):
-	inputpdf = PyPDF2.PdfFileReader(pdf_in_file)
-	
-	output = PyPDF2.PdfFileWriter()
+    inputpdf = PyPDF2.PdfFileReader(pdf_in_file)
     
-	output.addPage(inputpdf.getPage(i))
-	output.encrypt('password')
+    output.addPage(inputpdf.getPage(i))
+    output.encrypt('password')
 
-	with open("simple_password_protected.pdf", "wb") as outputStream:
-		output.write(outputStream)
+    #with open("simple_password_protected.pdf", "wb") as outputStream:
+    with open("gre_research_validity_data_password_protected.pdf", "wb") as outputStream:
+        output.write(outputStream)
 
 pdf_in_file.close()
